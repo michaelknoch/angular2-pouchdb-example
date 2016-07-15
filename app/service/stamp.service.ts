13d85f048ff35587372c928232b32e68d3c3c6d1
Michael Knoch <info@michaelknoch.de>
2016-07-15 14:06:55 +0200

    implement stampservice

---
+++ app/service/stamp.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/observable';
+import {PouchService} from "./pouch.service";
+
+@Injectable()
+export class StampService {
+
+    constructor(private _pouch: PouchService) {
+
+    }
+
+    public newStamp(stamp: any) {
+        return this._pouch.newStamp(stamp);
+    }
+
+    public getStamps() {
+        return this._pouch.getStamps();
+    }
+
+}
